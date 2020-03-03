d5228383a9d53cd5089b7031f2cd6d70bd2b64da
noname <you@example.com>
2020-03-03 22:24:55 +0100

    aas

--- TP2/q1.sql
+++ TP2/q1.sql
@@ -1,41 +1,51 @@
-17 contraintes SQL
-2 contraintes non SQL
-30 en pseudo SQL
+-------------------------------------------------------------------------------
+-- corr_contraintes.sql 
+-- tourne le 23 janvier 2018
+-------------------------------------------------------------------------------
+/* Contraintes SQL et non SQL. 
 
-a) contraintes SQL
+Contraintes SQL (bonus en partiel/examen si presentation comme suit) :
 
-Client:
-idc pk
-nom not null
-age not null check (16 <= age <= 123)
-avoir not null check (0 <= avoir <= 2000)
+client :
+    idc pk
+    nom not null
+    age not null check 16<=age<=120
+    avoir not null check 0<=avoir<=2000
 
-Village:
-idv pk
-ville not null
-activite
-prix not null check (0 < prix < 2000)
-capacite check (0<= capacite <= 100)
+village :
+    idv pk
+    ville not null
+    activite -- null possible
+    prix not null check 0<prix<=2000
+    capacite not null check 1<=capacite<=1000
 
-Sejour:
-ids not null pk
-idc not null fk ref Client
-idv not nullfk ref Village
-jour not null check (1 <= jour and jour <= 365)
-unique(idc,jour)
+sejour :
+    ids pk
+    idc fk client not null -- rappel : fk n'implique pas not null
+    idv fk village not null
+    jour not null check 1<=jour<=365
+    (idc, jour) unique 
 
-/*
-b) contraintes non SQL
+creer sequences pour client, village, sejour
 
-- le nombre de sejours pour un centre pour un jour ne peut pas dépasser sa capacité:
-	pour tout idv i de capacité n dans village
-	il y a au plus n lignes avec idv i pour chaque jour j dans sejour
-	
-	=> dans la table Sejour, pour un meme idv i et un meme jour j,
-	le nb de lignes qui contiennent ces couples (i,j) ne doit pas dépaser la capacite n
-	dans la table village.
+Contraintes non SQL : 
 
-- l'avoir d'un client plus la somme des prix de ses sejours ne peut dépasser 2000
+1. le nombre de sejours pour un centre pour un jour ne peut pas
+depasser sa capacite :
+
+  pour tout idv i de capacite n dans village,
+  il y a au plus n lignes avec idv i pour chaque jour j dans sejour
+
+2. l'avoir d'un client plus la somme des prix de ses sejours ne peut
+exceder 2000 :
+
+  pour tout idc i dans client : avoir + S <= 2000
+  en effet, un client part de 2000 et achete des sejours, donc :
+  avoir + somme des prix de ses sejours presents + somme des prix de
+  ses sejours detruits = 2000 ;
+  donc avoir + somme des prix de ses sejours presents <= 2000 ;
+  comment obtient-on S : considerons toutes les lignes d'idc i dans
+  sejour, et pour chacune, a partir de sa colonne idv dans sejour, son
+  prix dans village identifie par idv ; on note S la somme de tous ces
+  prix 
 */
-
-
